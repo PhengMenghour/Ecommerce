af07883fdf261fc45090d1bda20e1eb165a700ed
PhengMenghour <phengmenghour@gmail.com>
2024-11-18 22:18:55 +0700

    Add NavigationBarComponent Working on navigation bar (50% done)

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
-<script setup>
-
-</script>
-
 <template>
-
+    <NavigationBarComponent/>
 </template>
 
+<script>
+    import NavigationBarComponent from "./components/NavigationBarComponent.vue"
+
+    export default{
+        components: {
+            NavigationBarComponent
+        },
+    }
+
+</script>
 <style scoped>
 
 </style>
