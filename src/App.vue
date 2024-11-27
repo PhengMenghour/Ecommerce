0e9cb6f257b0c922d4dedd10b401299dfa7dd4f8
sion monika <sionmonoco@gmail.com>
2024-11-27 15:09:18 +0700

    finished  two components

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,19 @@
 <template>
     <NavigationBarComponent/>
+    <productCard/>
+    <items/>
 </template>
 
 <script>
     import NavigationBarComponent from "./components/NavigationBarComponent.vue"
-
+    import productCard from "./components/productCard.vue"
+    import items from "./components/items.vue"
     export default{
         components: {
-            NavigationBarComponent
+            NavigationBarComponent,
+            productCard,
+            items,
+
         },
     }
 
