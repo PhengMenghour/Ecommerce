182febd2d17575f87c529f8172d881da2cc63be6
Sothey <nithsothey2003@gmail.com>
2024-12-04 11:23:15 +0700

    Finished Components FeatureProduct and FeedbackComponent.

--- src/components/CategoryComponent.vue
+++ src/components/CategoryComponent.vue
@@ -34,7 +34,6 @@ export default {
         box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
         width: 180px;
         height: 225px;
-
         transition: transform .2s;
     }
 
--- src/views/HomePage.vue
+++ src/views/HomePage.vue
@@ -17,20 +17,45 @@
       <CategoryComponent :image="category.image" :title="category.title" />
     </div>
   </div>
+  <div class="feature-product-header">
+    <h1>Featured Products</h1>
+    <div class="featured-products">
+        <div class="products">
+          <ProductDiscount
+            v-for="product in products"
+            :key="product.id"
+            :name="product.name"
+            :image="product.image"
+            :price="product.price"
+            :originalPrice="product.originalPrice"
+            :discount="product.discount"
+            :reviews="product.reviews"
+          />
+        </div>
+      <button class="view-all-button">View All Products</button>
+    </div>
+  </div>
+
+  <div class="feedback-header">
+    <h1>Users Feedback</h1>
+    <FeedbackComponent/>
+  </div>
+
 </template>
 
 <script>
 import CategoryComponent from "@/components/CategoryComponent.vue";
 import TagComponent from "@/components/TagComponent.vue";
- 
+import ProductDiscount from "@/components/FeatureProduce.vue";
+import FeedbackComponent from "@/components/FeedbackComponent.vue";
 
 export default {
   components: {
     CategoryComponent,
     TagComponent,
+    ProductDiscount,
+    FeedbackComponent,
   },
-
-
 
   computed: {
     visibleCategories() {
@@ -91,6 +116,87 @@ export default {
           id: 7,
           image: "/src/assets/images/headphones.png",
           title: "Headphone",
+        },
+      ],
+      
+      products: [
+        {
+          id: 1,
+          name: "IPhone 16 Pro Max",
+          image: "./src/assets/images/iphone_16_pro_max.jpg",
+          price: 1249.99,
+          originalPrice: 1499.99,
+          discount: 10,
+          reviews: 103,
+        },
+
+        {
+          id: 2,
+          name: "Galaxy S24 Ultra",
+          image: "./src/assets/images/galaxy_s24_ultra.jpg",
+          price: 1299.99,
+          originalPrice: 0,
+          discount: 0,
+          reviews: 99,
+        },
+
+        {
+          id: 3,
+          name: "Xiaomi 14 Ultra",
+          image: "./src/assets/images/xiaomi_14ultra.jpg",
+          price: 699.99,
+          originalPrice: 0,
+          discount: 0,
+          reviews: 99,
+        },
+
+        {
+          id: 4,
+          name: "Vivo X100 Pro",
+          image: "./src/assets/images/vivo_x100pr0.jpg",
+          price: 499.99,
+          originalPrice: 549.99,
+          discount: 10,
+          reviews: 103,
+        },
+      ],
+
+      products: [
+        {
+          id: 1,
+          name: "IPhone 16 Pro Max",
+          image: "./src/assets/images/iphone_16_promax.jpg",
+          price: 1249.99,
+          originalPrice: 1499.99,
+          discount: 10,
+          reviews: 103,
+        },
+        {
+          id: 2,
+          name: "Galaxy S24 Ultra",
+          image: "./src/assets/images/galaxy_s24_ultra.jpg",
+          price: 1299.99,
+          originalPrice: 0,
+          discount: 0,
+          reviews: 99,
+        },
+        {
+          id: 3,
+          name: "Xiaomi 14 Ultra",
+          image: "./src/assets/images/xiaomi_14ultra.jpg",
+          price: 699.99,
+          originalPrice: 0,
+          discount: 0,
+          reviews: 99,
+        },
+        {
+          id: 4,
+          name: "Vivo X100 Pro",
+          image: "./src/assets/images/vivo_x100pro.jpg",
+          price: 499.99,
+          originalPrice: 549.99,
+          discount: 10,
+          reviews: 103,
         },
       ],
     };
@@ -155,4 +261,46 @@ export default {
   margin-top: 10px;
   margin-left: 200px;
 }
+
+.feature-product-header {
+  margin: 0px 200px 0px 200px;
+  font-family: "Poppins", sans-serif;
+}
+
+.featured-products {
+  text-align:center;
+  padding: 16px;
+  font-family: "Poppins";
+  font-weight: 200;
+  margin-top: 30px; 
+}
+
+.products {
+  display: flex;
+  gap: 16px;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.view-all-button {
+  margin-top: 20px;
+  padding: 15px 30px;
+  background-color:#e5c5ba;
+  color: black;
+  font-weight: bold;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+.view-all-button:hover{
+  background-color: #b5958a;
+  transform: scale(1.05);
+  transition: transform .1s;
+}
+
+.feedback-header {
+  margin: 0px 200px 0px 200px;
+  font-family: "Poppins", sans-serif;
+}
 </style>
+
