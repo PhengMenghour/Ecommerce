07fc09ce0e337b802ffc001829e230dd9e85056e
sion monika <sionmonoco@gmail.com>
2024-12-09 09:53:44 +0700

    product card

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,7 @@ import ContactPage from '@/views/ContactPage.vue'
 import ShopPage from '@/views/ShopPage.vue'
 import Pages from '@/views/Pages.vue'
 import SignInPage from '@/views/SignInPage.vue'
+import ProductDetail from '@/views/ProductDetail.vue'
 
 const routes = [
   {
@@ -30,6 +31,11 @@ const routes = [
     path: '/signIn',
     name: 'signIn',
     component: SignInPage,
+  },
+  {
+    path: '/productDetail',
+    name: 'productDetail',
+    component: ProductDetail,
   }
 ]
 
