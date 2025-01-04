2541099d0c0fd744354b511f14090382d25c122f
SOEUNGPHEAREAKPANHABOTHGIC <bothpanha553@gmail.com>
2025-01-04 11:04:25 +0700

    Do AddCard and checkout page

---
+++ src/components/AddCard/OrderSummary.vue
@@ -0,0 +1,22 @@
+<template>
+    <div>
+      <h2>Your Order</h2>
+      <ul>
+        <li v-for="item in orderItems" :key="item.name">
+          <span>{{ item.name }}</span>
+          <span>${{ item.price.toFixed(2) }}</span>
+        </li>
+        <li class="total">
+          <span>Total</span>
+          <span>${{ totalPrice.toFixed(2) }}</span>
+        </li>
+      </ul>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    props: ['orderItems', 'totalPrice']
+  };
+  </script>
+  
