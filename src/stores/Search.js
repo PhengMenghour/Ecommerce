ab1516e81ee477c3c74981c3c0acf319671e326a
Sothey <nithsothey2003@gmail.com>
2025-01-12 21:05:29 +0700

    finished search and fix listing products

---
+++ src/stores/Search.js
@@ -0,0 +1,13 @@
+// stores/Search.js
+import { defineStore } from 'pinia';
+
+export const useSearchStore = defineStore('search', {
+  state: () => ({
+    query: '',
+  }),
+  actions: {
+    setQuery(newQuery) {
+      this.query = newQuery;
+    },
+  },
+});
